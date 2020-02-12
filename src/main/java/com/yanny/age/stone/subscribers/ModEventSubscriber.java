d8fde7cba228b8428daf7f858f248038b682ce2a
Jan Lalis <lalis.jan@gmail.com>
2020-02-12 21:52:39 +0100

    Fixed build

--- src/main/java/com/yanny/age/stone/subscribers/ModEventSubscriber.java
+++ src/main/java/com/yanny/age/stone/subscribers/ModEventSubscriber.java
@@ -12,7 +12,6 @@ import net.minecraft.world.gen.GenerationStage;
 import net.minecraft.world.gen.feature.ProbabilityConfig;
 import net.minecraft.world.gen.placement.IPlacementConfig;
 import net.minecraft.world.gen.placement.Placement;
-import net.minecraftforge.client.event.ColorHandlerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
@@ -23,12 +22,9 @@ import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.registries.ForgeRegistries;
 
 import static com.yanny.age.stone.Reference.MODID;
-import static com.yanny.age.stone.subscribers.ToolSubscriber.*;
 
 @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ModEventSubscriber {
-
-    private static final int OVERLAY_BONE_COLOR = 0xe8e5d2;
 
     @SubscribeEvent
     public static void init(FMLCommonSetupEvent event) {
@@ -81,11 +77,5 @@ public class ModEventSubscriber {
                                 func_227228_a_(Placement.NOPE.func_227446_a_(IPlacementConfig.NO_PLACEMENT_CONFIG)));
             }
         }
-    }
-
-    @SubscribeEvent
-    public static void onColorRegister(ColorHandlerEvent.Item event) {
-        event.getItemColors().register((itemStack, index) -> OVERLAY_BONE_COLOR, bone_axe_head, bone_pickaxe_head, bone_hoe_head, bone_shovel_head, bone_sword_head);
-        event.getItemColors().register((itemStack, index) -> index == 0 ? OVERLAY_BONE_COLOR : -1, bone_axe, bone_pickaxe, bone_hoe, bone_shovel, bone_sword);
     }
 }
