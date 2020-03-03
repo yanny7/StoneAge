7a7a7c4fe3806b852d0e69b35278a051998d8c3b
Jan Lalis <lalis.jan@gmail.com>
2020-03-03 16:50:23 +0100

    Fixed injection in 1.15

--- src/main/java/com/yanny/age/stone/handlers/LootHandler.java
+++ src/main/java/com/yanny/age/stone/handlers/LootHandler.java
@@ -74,6 +74,7 @@ public class LootHandler {
         INJECT_POOL.put("minecraft:entities/sheep", "stone_age:inject/entities/bone_fat");
 
         INJECT_ITEM_POOL.put("minecraft:blocks/seagrass", new Pair<>(Blocks.SEAGRASS, Tags.Items.SHEARS));
+        INJECT_ITEM_POOL.put("minecraft:blocks/tall_seagrass", new Pair<>(Blocks.SEAGRASS, Tags.Items.SHEARS));
         INJECT_ITEM_POOL.put("minecraft:blocks/vine", new Pair<>(Blocks.VINE, Tags.Items.SHEARS));
 
         INJECT_ALTERNATE_ENTRIES.put("minecraft:blocks/stone", new Pair<>(Blocks.GRAVEL, Tags.Items.HAMMERS));
@@ -89,7 +90,6 @@ public class LootHandler {
         INJECT_ALTERNATE_ENTRIES.put("minecraft:blocks/oak_leaves", new Pair<>(Blocks.OAK_LEAVES, Tags.Items.SHEARS));
         INJECT_ALTERNATE_ENTRIES.put("minecraft:blocks/spruce_leaves", new Pair<>(Blocks.SPRUCE_LEAVES, Tags.Items.SHEARS));
         INJECT_ALTERNATE_ENTRIES.put("minecraft:blocks/tall_grass", new Pair<>(Blocks.GRASS, Tags.Items.SHEARS));
-        INJECT_ALTERNATE_ENTRIES.put("minecraft:blocks/tall_seagrass", new Pair<>(Blocks.SEAGRASS, Tags.Items.SHEARS));
 
         CHANGE_ALTERNATE_ENTRIES.put("minecraft:blocks/sandstone", new Pair<>(Blocks.SAND, Tags.Items.HAMMERS));
         CHANGE_ALTERNATE_ENTRIES.put("minecraft:blocks/chiseled_sandstone", new Pair<>(Blocks.SAND, Tags.Items.HAMMERS));
