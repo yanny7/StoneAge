5321e7df7776285c19aecaef8306b7fa9ec4ccd7
Jan Lalis <lalis.jan@gmail.com>
2020-06-14 22:29:10 +0200

    Fixed problem with compatibility with inventory sorter mod

--- src/main/java/com/yanny/age/stone/blocks/FeederContainer.java
+++ src/main/java/com/yanny/age/stone/blocks/FeederContainer.java
@@ -5,6 +5,7 @@ import com.yanny.age.stone.subscribers.BlockSubscriber;
 import com.yanny.age.stone.subscribers.ContainerSubscriber;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.container.Container;
 import net.minecraft.inventory.container.Slot;
 import net.minecraft.item.ItemStack;
@@ -13,9 +14,7 @@ import net.minecraft.util.IWorldPosCallable;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.items.CapabilityItemHandler;
-import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.items.SlotItemHandler;
-import net.minecraftforge.items.wrapper.InvWrapper;
 
 import javax.annotation.Nonnull;
 
@@ -24,7 +23,7 @@ import static com.yanny.age.stone.blocks.FeederTileEntity.ITEMS;
 public class FeederContainer extends Container {
     private final FeederTileEntity tile;
     private final PlayerEntity player;
-    private final IItemHandler inventory;
+    private final IInventory inventory;
 
     public FeederContainer(int windowId, PlayerInventory inv, PacketBuffer extraData) {
         this(windowId, extraData.readBlockPos(), ExampleMod.proxy.getClientWorld(), inv, ExampleMod.proxy.getClientPlayer());
@@ -34,7 +33,7 @@ public class FeederContainer extends Container {
         super(ContainerSubscriber.feeder, id);
         tile = (FeederTileEntity) world.getTileEntity(pos);
         this.player = player;
-        this.inventory = new InvWrapper(inventory);
+        this.inventory = inventory;
 
         if (tile == null) {
             throw new IllegalStateException("TileEntity does not exists!");
@@ -100,9 +99,9 @@ public class FeederContainer extends Container {
         super.onContainerClosed(playerIn);
     }
 
-    private int addSlotRange(IItemHandler handler, int index, int x, int y, int amount, int dx) {
+    private int addSlotRange(IInventory handler, int index, int x, int y, int amount, int dx) {
         for (int i = 0 ; i < amount ; i++) {
-            addSlot(new SlotItemHandler(handler, index, x, y));
+            addSlot(new Slot(handler, index, x, y));
             x += dx;
             index++;
         }
@@ -111,7 +110,7 @@ public class FeederContainer extends Container {
     }
 
     @SuppressWarnings("SameParameterValue")
-    private void addSlotBox(IItemHandler handler, int index, int x, int y, int horAmount, int dx, int verAmount, int dy) {
+    private void addSlotBox(IInventory handler, int index, int x, int y, int horAmount, int dx, int verAmount, int dy) {
         for (int j = 0 ; j < verAmount ; j++) {
             index = addSlotRange(handler, index, x, y, horAmount, dx);
             y += dy;
--- src/main/java/com/yanny/age/stone/blocks/FishingNetContainer.java
+++ src/main/java/com/yanny/age/stone/blocks/FishingNetContainer.java
@@ -5,6 +5,7 @@ import com.yanny.age.stone.subscribers.BlockSubscriber;
 import com.yanny.age.stone.subscribers.ContainerSubscriber;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.container.Container;
 import net.minecraft.inventory.container.Slot;
 import net.minecraft.item.ItemStack;
@@ -13,9 +14,7 @@ import net.minecraft.util.IWorldPosCallable;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.items.CapabilityItemHandler;
-import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.items.SlotItemHandler;
-import net.minecraftforge.items.wrapper.InvWrapper;
 
 import javax.annotation.Nonnull;
 
@@ -24,7 +23,7 @@ import static com.yanny.age.stone.blocks.FishingNetTileEntity.*;
 public class FishingNetContainer extends Container {
     private final FishingNetTileEntity tile;
     private final PlayerEntity player;
-    private final IItemHandler inventory;
+    private final IInventory inventory;
 
     public FishingNetContainer(int windowId, PlayerInventory inv, PacketBuffer extraData) {
         this(windowId, extraData.readBlockPos(), ExampleMod.proxy.getClientWorld(), inv, ExampleMod.proxy.getClientPlayer());
@@ -34,7 +33,7 @@ public class FishingNetContainer extends Container {
         super(ContainerSubscriber.fishing_net, id);
         tile = (FishingNetTileEntity) world.getTileEntity(pos);
         this.player = player;
-        this.inventory = new InvWrapper(inventory);
+        this.inventory = inventory;
 
         if (tile == null) {
             throw new IllegalStateException("TileEntity does not exists!");
@@ -99,9 +98,9 @@ public class FishingNetContainer extends Container {
         return ItemStack.EMPTY;
     }
 
-    private int addSlotRange(IItemHandler handler, int index, int x, int y, int amount, int dx) {
+    private int addSlotRange(IInventory handler, int index, int x, int y, int amount, int dx) {
         for (int i = 0 ; i < amount ; i++) {
-            addSlot(new SlotItemHandler(handler, index, x, y));
+            addSlot(new Slot(handler, index, x, y));
             x += dx;
             index++;
         }
@@ -110,7 +109,7 @@ public class FishingNetContainer extends Container {
     }
 
     @SuppressWarnings("SameParameterValue")
-    private void addSlotBox(IItemHandler handler, int index, int x, int y, int horAmount, int dx, int verAmount, int dy) {
+    private void addSlotBox(IInventory handler, int index, int x, int y, int horAmount, int dx, int verAmount, int dy) {
         for (int j = 0 ; j < verAmount ; j++) {
             index = addSlotRange(handler, index, x, y, horAmount, dx);
             y += dy;
